615cafe683a16d458344161505498c1780643cac
unknown <mudithadilshan1999@gmail.com>
2023-02-01 21:30:59 +0530

    start

---
+++ Bordima/resources/views/index.blade.php
@@ -0,0 +1,54 @@
+<x-layout>
+  <section class="bg-blue-800 border-t-2">
+    <div class="flex p-2.5">
+      <div class="text-xl text-center w-1/3">
+        <input class="bg-yellow-300 font-semibold rounded-2xl text-center" type="text" name="one" id="" value="filter one">
+      </div>
+      <div class="text-xl text-center w-1/3">
+        <input class="bg-yellow-300 font-semibold rounded-2xl text-center" type="text" name="one" id="" value="filter one">
+      </div>
+      <div class="text-xl text-center w-1/3">
+        <input class="bg-yellow-300 font-semibold rounded-2xl text-center" type="text" name="one" id="" value="filter one">
+      </div>
+    </div>
+  </section>
+
+  <section class="m-10">
+    <div class="flex">
+      <div class="p-2.5 hover:bg-white rounded-xl p-2.5">
+        <div><img src="images/illustration-1.png" alt="post" width="350px">
+        </div>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime dignissimos 
+          
+          
+        </div>
+        <div class="mt-5 text-center">
+          <button type="submit" class="bg-red-600 hover:bg-red-400 p-1.5 rounded-2xl text-2xl text-white w-1/3">view</button>
+        </div>
+      </div>
+      <div class="p-2.5 hover:bg-white rounded-xl p-2.5">
+        <div><img src="images/illustration-1.png" alt="post" width="350px">
+        </div>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime dignissimos 
+         
+        </div>
+        <div class="mt-5 text-center">
+          <button type="submit" class="bg-red-600 hover:bg-red-400 p-1.5 rounded-2xl text-2xl text-white w-1/3">view</button>
+        </div>
+      </div>
+      <div class="p-2.5 hover:bg-white rounded-xl p-2.5">
+        <div><img src="images/illustration-1.png" alt="post" width="350px">
+        </div>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime dignissimos 
+          
+        </div>
+        <div class="mt-5 text-center">
+          <button type="submit" class="bg-red-600 hover:bg-red-400 p-1.5 rounded-2xl text-2xl text-white w-1/3">view</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</x-layout>
